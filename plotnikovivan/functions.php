5e3b38a3a5138ee420c6e3c34cd75fcf2ac046c6
Johnplot <info@motorsnews.ru>
2020-02-22 19:58:23 +0300

    create tamplate

--- plotnikovivan/functions.php
+++ plotnikovivan/functions.php
@@ -5,3 +5,12 @@ add_action( 'wp_enqueue_scripts', 'style_theme');
 function style_theme() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
+
+
+function font_awesome() {
+    if (!is_admin()) {
+      wp_register_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css');
+      wp_enqueue_style('font-awesome');
+    }
+  }
+  add_action('wp_enqueue_scripts', 'font_awesome');
--- plotnikovivan/index.php
+++ plotnikovivan/index.php
@@ -1,8 +1,11 @@
 <? get_header(); ?>
 
 <!-- start main -->
+<section>
 
 
+
+</section>
 <!-- end main -->
 
 <? get_footer(); ?>
